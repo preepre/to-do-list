a5f9f4832ae0cab4955888896f8e349b51db7d22
Priya Prabhakar <Priya.Prabhakar@LibertyMutual.com>
2017-08-24 14:33:23 -0700

    Submitting mini project.

--- src/main/java/com/libertymutual/goforcode/todolist/services/ToDoItemRepository.java
+++ src/main/java/com/libertymutual/goforcode/todolist/services/ToDoItemRepository.java
@@ -1,17 +1,13 @@
 package com.libertymutual.goforcode.todolist.services;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
@@ -21,119 +17,167 @@ import com.libertymutual.goforcode.todolist.models.ToDoItem;
 @Service
 public class ToDoItemRepository {
 
-    private int nextId = 1;
-    private ToDoItem todo = new ToDoItem();
-    private List<ToDoItem> items;
-    
+	private int nextId = 1;
 
-    /**
-     * Get all the items from the file. 
-     * @return A list of the items. If no items exist, returns an empty list.
-     */
-    public List<ToDoItem> getAll() {
-    	
-    	
-    	try (Reader in = new FileReader("to-do-list.csv") ) {
-    		
-    		 CSVParser parser = CSVFormat.EXCEL.parse(in);
-    		 
-    		 List<CSVRecord> list = parser.getRecords();   		
-    		 
-    		 for ( CSVRecord record : parser) {
-    			 
-    			 String id = record.get("id");
-    			 String text = record.get("text");
-    			 String complete = record.get("isComplete");
-    			 
-    		 }
-    		 
-//    		 CSVPrinter printer = new CSVPrinter('/', parser);
-    		 
-    		 
-    		 
-    		 
+	/**
+	 * Get all the items from the file.
+	 * 
+	 * @return A list of the items. If no items exist, returns an empty list.
+	 */
+	public List<ToDoItem> getAll() {
 
-    		
-    		
-    		//Scanner scanner = new Scanner(); 
-    		
+		List<ToDoItem> items = new ArrayList<ToDoItem>();
 
-//    		
-//    		CSVParser parser = new CSVParser(in, formatter );
-//    		null
-//    		CSVPrinter printer = new CSVPrinter(, formatter);
+		try (Reader in = new FileReader("to-do-list.csv")) {
 
-    		
-//    		BufferedReader buffy = new BufferedReader(in);
-//    		
-//    		String line = buffy.readLine();
-//    		ToDoItem item = new ToDoItem();
-//    		
-//    		while(line != null ) {
-//    			
-//    		}
-    		
+			Iterable<CSVRecord> records = null;
+			records = CSVFormat.DEFAULT.parse(in);
 
-    		
-    	}
-    	catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-    	
-    	catch (IOException e) {
+			for (CSVRecord record : records) {
+
+				ToDoItem newItem = new ToDoItem();
+
+				newItem.setId(Integer.parseInt(record.get(0)));
+				newItem.setText(record.get(1));
+				newItem.setComplete(Boolean.parseBoolean(record.get(2)));
+
+				items.add(newItem);
+
+				// String id = record.get("id");
+				// String text = record.get("text");
+				// String complete = record.get("isComplete");
+				//
+				// newItem.setId(Integer.parseInt(id));
+				// newItem.setText(text);
+				// newItem.setComplete(Boolean.parseBoolean(complete));
+				//
+				// items.add(newItem);
+
+			}
+
+			// in the same for loop
+			// create a to do item
+			// set the to do item properties
+			// need to convert from String to Integer
+			// return the to do item
+
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    	
-        // Replace this with something meaningful
-        return Collections.emptyList();
-    }
 
-    /**
-     * Assigns a new id to the ToDoItem and saves it to the file.
-     * @param item The to-do item to save to the file.
-     */
-    public void create(ToDoItem item) {
-        // Fill this in with something meaningful
-    	
-    	item.setId(nextId);
-    	nextId ++;
-    	
-    	FileWriter writer;
-		try {
-			writer = new FileWriter("to-do-list.csv");
-	    	CSVPrinter printer = CSVFormat.DEFAULT.print(writer);
-	    	String[] fullRecord = {"id", "text", "isComplete"}; //is this right?!
-	    	
-	    	printer.printRecord(fullRecord);
-	    	
-	    	//printer.printRecord(//insert stuff here);
-	    	
-	    	
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Replace this with something meaningful
+		return items;
+
+		// return Collections.emptyList();
+	}
+
+	/**
+	 * Assigns a new id to the ToDoItem and saves it to the file.
+	 * 
+	 * @param item
+	 *            The to-do item to save to the file.
+	 */
+	public void create(ToDoItem item) {
+		// Fill this in with something meaningful
+
+		item.setId(nextId);
+		nextId++;
+
+		String file = "to-do-list.csv";
+
+		CSVFormat format = CSVFormat.DEFAULT.withHeader();
+
+		try (FileWriter writer = new FileWriter(file, true); CSVPrinter printer = CSVFormat.DEFAULT.print(writer)) {
+
+			String[] fullRecord = { Integer.toString(item.getId()), item.getText(),
+					Boolean.toString(item.isComplete()) };
+			// String[] fullRecord = { "id", "text", "isComplete" }; // is this right?!
+
+			printer.printRecord(fullRecord);
+
+			// printer.printRecord(//insert stuff here);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    /**
-     * Gets a specific ToDoItem by its id.
-     * @param id The id of the ToDoItem.
-     * @return The ToDoItem with the specified id or null if none is found.
-     */
-    public ToDoItem getById(int id) {
-        // Replace this with something meaningful
-        return null;
-    }
+	/**
+	 * Gets a specific ToDoItem by its id.
+	 * 
+	 * @param id
+	 *            The id of the ToDoItem.
+	 * @return The ToDoItem with the specified id or null if none is found.
+	 */
+	public ToDoItem getById(int id) {
+		// Replace this with something meaningful
+		List<ToDoItem> existingToDoList = getAll();
 
-    /**
-     * Updates the given to-do item in the file.
-     * @param item The item to update.
-     */
-    public void update(ToDoItem item) {
-        // Fill this in with something meaningful
-    }
+		for (ToDoItem findItem : existingToDoList) {
+			if (findItem.getId() == id) {
+				return findItem;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Updates the given to-do item in the file.
+	 * 
+	 * @param item
+	 *            The item to update.
+	 */
+	public void update(ToDoItem item) {
+		// Fill this in with something meaningful
+
+		// Find an item in file by ID
+		// Update item with what the new item properties are - text and isComplete
+		// Save list
+
+		// Create two lists - one with current items, and another with future items
+		// Future list has up to date information
+		// Update file with future list
+
+		List<ToDoItem> existingToDoList = getAll();
+//		List<ToDoItem> updatedToDoList = new ArrayList<ToDoItem>();
+
+		int idIncrementer = 1;
+		String file = "to-do-list.csv";
+//		CSVFormat format = CSVFormat.DEFAULT.withHeader();
+
+		try (FileWriter writer = new FileWriter(file, false); CSVPrinter printer = CSVFormat.DEFAULT.print(writer)) {
+
+			for (ToDoItem findItem : existingToDoList) {
+
+				if (findItem.getId() == item.getId()) {
+
+					findItem.setComplete(true);
+
+				}
+
+
+				findItem.setId(idIncrementer);
+				idIncrementer+=1;
+
+				String[] newRecord = { Integer.toString(findItem.getId()), findItem.getText(),
+						Boolean.toString(findItem.isComplete()) };
+
+				printer.printRecord(newRecord);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); 
+		}
+
+	}
 
 }
